516004f461d11601eac5ffbc08b821829b27b158
Te.undeaD_D <8116188+undeaDD@users.noreply.github.com>
2021-08-21 14:37:08 +0200

    Update OneTextFieldViewController.swift

--- Source/Pickers/TextFields/OneTextFieldViewController.swift
+++ Source/Pickers/TextFields/OneTextFieldViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-extension UIAlertController {
+public extension UIAlertController {
     
     /// Add a textField
     ///
@@ -10,7 +10,7 @@ extension UIAlertController {
     ///   - vInset: bottom margin to button
     ///   - configuration: textField
     
-    func addOneTextField(configuration: TextField.Config?) {
+    public func addOneTextField(configuration: TextField.Config?) {
         let textField = OneTextFieldViewController(vInset: preferredStyle == .alert ? 12 : 0, configuration: configuration)
         let height: CGFloat = OneTextFieldViewController.ui.height + OneTextFieldViewController.ui.vInset
         set(vc: textField, height: height)
